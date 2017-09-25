b6937a18165a7ea4df6a161f43126b3aafd0a627
Daniel Kim <dkim099@gmail.com>
2017-09-25 11:53:50 -0700

    db conncetion

--- server/db/config.js
+++ server/db/config.js
@@ -1,8 +1,16 @@
 const mongoose = require('mongoose');
 
-const mongoDB = 'mongodb://127.0.0.1/test';
+const mongoDB = 'mongodb://dan:plantlife@ds149974.mlab.com:49974/acrud'
 
-const db = mongoose.connection;
+const db = mongoose.createConnection(mongoDB, { config: {autoIndex: false} }, (err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('connected to db')
+    }
+});
+
+
 const Schema = mongoose.Schema;
 
 const orderInfo = new Schema({
@@ -18,7 +26,7 @@ const user = new Schema({
 const Order = mongoose.model('Order', orderInfo);
 
 module.exports = {
-    Order
+    db
 }
 
 
