02b9acadaa3066fd729f6c251336dd48ba58e68c
Nadina Gerlach <nadina@nadina.la>
2017-09-25 15:45:22 -0700

    stub auth service with firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,13 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+// Firebase 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { firebaseConfig } from '../environments/firebase.config';
+import { AuthService } from './services/auth.service';
+
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LoginComponent } from './login/login.component';
@@ -38,10 +45,14 @@ import { HomeComponent } from './home/home.component';
     BrowserModule,
     NgbModule.forRoot(),
     FormsModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
     HttpModule
   ],
-  providers: [],
+  providers: [AuthService],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
